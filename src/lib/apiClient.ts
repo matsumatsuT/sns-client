470512509612ac3dcf185d05af1e3464d29f601f
matsumoto <dsongben@gmail.com>
2023-08-16 16:35:43 +0900

    環境変数の設定

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,12 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  // ローカル開発時はこちらを使用する
-  baseURL: "http://localhost:8000/api",
-
-  // 本番環境時はこちらを使用する
-  // baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
-
+  baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
   headers: {
     "Content-Type": "application/json"
   },
